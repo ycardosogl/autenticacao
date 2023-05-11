3ba464a50bf597ce3eed5da723645d5c0e198f46
ycardosogl <ycardoso050@gmail.com>
2023-05-11 16:19:50 -0400

    11/05

--- frontend/src/paginas/cadastro.js
+++ frontend/src/paginas/cadastro.js
@@ -1,24 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Nav, Button, Col, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function Cadastro(){
+
+  const [formData, setFormData] = useState({
+    email: '',
+    nome: '',
+    senha: '',
+    nrsec: ''
+  });
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:5001/users', formData);
+      console.log(response.data);
+      alert('inserido!');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
     return(
         <div class="container">
         <h1>Formulário de Cadastro</h1>
         <form>
           <div class="form-group">
             <label for="name">Nome:</label>
-            <input type="text" id="name" name="name" required/>
+            <input type="text" id="nome" name="nome" value={formData.nome} onChange={handleChange} required/>
           </div>
           <div class="form-group">
             <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required/>
+            <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required/>
           </div>
           <div class="form-group">
             <label for="password">Senha:</label>
-            <input type="password" id="password" name="password" required/>
+            <input type="senha" id="password" name="senha" value={formData.senha} onChange={handleChange} required/>
           </div>
+          <div className="form-group">
+          <label htmlFor="nrsec">Número de segurança:</label>
+          <input type="text" id="nrsec" name="nrsec" value={formData.nrsec} onChange={handleChange} required/>
+        </div>
           <button type="submit" class="submit-btn">Cadastrar</button>
         </form>
       </div>
--- frontend/src/paginas/login.js
+++ frontend/src/paginas/login.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Nav, Button, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './styles.css'
 import Cadastro from './cadastro';
+import axios from 'axios';
 
 function Login(){
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(email, senha) 
+    
+    axios.post('http://localhost:5001/users/confirma', {email,senha})
+
+    .then(response => {
+      if(response.status === 200, 201) {
+        alert('Bem-vindo!');
+        setEmail('');
+        setSenha('');
+      }
+    })
+    .catch(error => console.log(error))
+  }
   return(
     
     <div class="wrapper fadeInDown">
@@ -12,9 +31,9 @@ function Login(){
 
              <h2 class="active"> Login </h2>
 
-                <form class="box-login" method="post" action="#">
-                    <input type="email" id="email" class="campo" name="email" placeholder="E-mail"/>
-                    <input type="text" id="password" class="campo" name="senha" placeholder="Senha"/>
+                <form class="box-login" method="post" action="#"  onSubmit={handleSubmit}>
+                    <input type="email" id="email" class="campo" name="email" placeholder="E-mail" value={email} onChange={e => setEmail(e.target.value)} required/>
+                    <input type="text" id="password" class="campo" name="senha" placeholder="Senha" value={senha} onChange={e => setSenha(e.target.value)} required/>
                     <input type="submit" class="botao" value="Entrar"/>
                 </form>
                 <div class="box-lembrar-senha">  
